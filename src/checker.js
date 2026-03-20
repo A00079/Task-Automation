306fb2264c0aca292602ed25fc2043d003d35fbe
ajaybendre <ajay.bendre@motilaloswal.com>
2026-03-20 10:08:47 +0530

    revert: restore original simple Railway setup

--- src/checker.js
+++ src/checker.js
@@ -386,12 +386,10 @@ async function checkNAVUpdate() {
   
   try {
     console.log('Launching browser...');
-    const launchOptions = {
+    browser = await puppeteer.launch({
       headless: 'new',
       args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage', '--disable-gpu']
-    };
-    if (process.env.PUPPETEER_EXECUTABLE_PATH) launchOptions.executablePath = process.env.PUPPETEER_EXECUTABLE_PATH;
-    browser = await puppeteer.launch(launchOptions);
+    });
     
     const page = await browser.newPage();
     const result = await checkNAVWithPage(page);
@@ -1276,12 +1274,10 @@ async function main(discordClient, userPAN, userPasscode, kfintechAUM, kfintechC
   let browser;
   try {
     console.log('Launching browser...');
-    const launchOptions = {
+    browser = await puppeteer.launch({
       headless: 'new',
       args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage', '--disable-gpu', '--disable-blink-features=AutomationControlled', '--window-size=1920,1080']
-    };
-    if (process.env.PUPPETEER_EXECUTABLE_PATH) launchOptions.executablePath = process.env.PUPPETEER_EXECUTABLE_PATH;
-    browser = await puppeteer.launch(launchOptions);
+    });
 
     // Check NAV
     const page = await browser.newPage();
